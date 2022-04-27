afa0ea316af0c990fc28e029c59413db77f0a7e3
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-04-27 13:28:05 +0500

    a

--- app.js
+++ app.js
@@ -4,107 +4,88 @@ const app = express();
 
 app.use(express.json()); //qorovulcha
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, {
-    encoding: 'utf-8',
-  })
+const magic = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/magic-soft-big.json`, 'utf-8')
 );
 
-const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/reviews.json`, {
-    encoding: 'utf-8',
-  })
-);
-
-// console.log(tours);
-
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/magic', (req, res) => {
   res.status(200).json({
     status: 'Success',
     data: {
-      tours,
+      magic,
     },
   });
 });
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      reviews,
-    },
-  });
-});
-
-app.post(`/api/v1/tours`, (req, res) => {
+console.log(typeof magic);
+app.post(`/api/v1/magic`, (req, res) => {
   const data = req.body;
-  const newId = tours[tours.length - 1].id + 1;
+  const yangiId = magic[magic.length - 1].id + 1;
 
-  const complateObj = Object.assign({ id: newId }, data);
-
-  tours.push(complateObj);
+  const qoshadiganId = Object.assign({ id: yangiId }, data);
+  magic.push(qoshadiganId);
 
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours.json`,
-    JSON.stringify(tours),
+    `${__dirname}/dev-data/data/magic-soft.json`,
+    JSON.stringify(magic),
     'utf-8',
     (err) => {
       res.status(201).json({
         status: 'success',
         data: {
-          tour: tours,
+          tour: magic,
         },
       });
     }
   );
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = +req.params.id;
-  const data = tours.find((val) => val.id === id);
+// app.get('/api/v1/magic/:id', (req, res) => {
+//   const id = +req.params.id;
+//   const data = magic.find((val) => val.id === id);
 
-  console.log(tours);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      data,
-    },
-  });
-});
+//   console.log(magic);
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       data,
+//     },
+//   });
+// });
 
-app.patch('/api/v1/tours/:id', (req, res) => {
-  const id = +req.params.id;
-  const data = tours.find((val) => val.id === id);
+// app.patch('/api/v1/magic/:id', (req, res) => {
+//   const id = +req.params.id;
+//   const data = magic.find((val) => val.id === id);
 
-  if (data) {
-    res.status(200).json({
-      status: 'success',
-      data: 'topdim',
-    });
-  } else if (!data) {
-    res.status(404).json({
-      status: 'success',
-      data: 'topolmadim',
-    });
-  }
-});
+//   if (data) {
+//     res.status(200).json({
+//       status: 'success',
+//       data: 'topdim',
+//     });
+//   } else if (!data) {
+//     res.status(404).json({
+//       status: 'success',
+//       data: 'topolmadim',
+//     });
+//   }
+// });
 
-app.delete('/api/v1/tours/:id', (req, res) => {
-  const id = +req.params.id;
-  const arr = tours.filter((val) => val.id != id);
+// app.delete('/api/v1/magic/:id', (req, res) => {
+//   const id = +req.params.id;
+//   const arr = magic.filter((val) => val.id != id);
 
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(arr),
-    'utf-8',
-    (err) => {
-      res.status(204).json({
-        status: 'success',
-        data: 'malumot ochirildi',
-      });
-    }
-  );
-});
+//   fs.writeFile(
+//     `${__dirname}/dev-data/data/magic-simple.json`,
+//     JSON.stringify(arr),
+//     'utf-8',
+//     (err) => {
+//       res.status(204).json({
+//         status: 'success',
+//         data: 'malumot ochirildi',
+//       });
+//     }
+//   );
+// });
 
 const port = 8003;
 app.listen(port, '127.0.0.1');
