2eedc5a387b4e2089e743a593970028417e27bec
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-05-04 17:54:18 +0500

    a

--- controller/tourCortroller.js
+++ controller/tourCortroller.js
@@ -1,49 +1,51 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
+// );
 
-const getTours = (req, res) => {
-  console.log(req.time);
+// const getTours = (req, res) => {
+//   console.log(req.time);
 
-  res.status(200).json({
-    status: 'Success',
-    message: 'hozircha ishlamaydi',
-  });
-};
+//   res.status(200).json({
+//     status: 'Success',
+//     message: 'hozircha ishlamaydi',
+//   });
+// };
 
-const getTour = (req, res) => {
-  console.log(req.time);
-  res.status(200).json({
-    status: 'Success',
-    message: 'hozircha ishlamaydi',
-  });
-};
+// const getTour = (req, res) => {
+//   console.log(req.time);
+//   res.status(200).json({
+//     status: 'Success',
+//     message: 'hozircha ishlamaydi',
+//   });
+// };
 
-const postTours = (req, res) => {
-  console.log(req.time);
+// const postTours = (req, res) => {
+//   console.log(req.time);
 
-  res.status(201).json({
-    status: 'Success',
-    message: 'hozircha ishlamaydi',
-  });
-};
+//   res.status(201).json({
+//     status: 'Success',
+//     message: 'hozircha ishlamaydi',
+//   });
+// };
 
-const patchTours = (req, res) => {
-  console.log(req.time);
-  res.status(200).json({
-    status: 'Success',
-    message: 'hozircha ishlamaydi',
-  });
-};
+// const patchTours = (req, res) => {
+//   console.log(req.time);
+//   res.status(200).json({
+//     status: 'Success',
+//     message: 'hozircha ishlamaydi',
+//   });
+// };
 
-const deleteTours = (req, res) => {
-  console.log(req.time);
-  res.status(204).json({
-    status: 'Success',
-    message: 'hozircha ishlamaydi',
-  });
-};
+// const deleteTours = (req, res) => {
+//   console.log(req.time);
+//   res.status(204).json({
+//     status: 'Success',
+//     message: 'hozircha ishlamaydi',
+//   });
+// };
 
-module.exports = { getTours, postTours, getTour, deleteTours, patchTours };
+// module.exports = { getTours, postTours, getTour, deleteTours, patchTours };
+
+// ///////////////////////////////////////////////////////
