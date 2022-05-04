a4615debf9b4ff6fc588afe530f909caa30f4ce5
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-05-04 16:30:48 +0500

    a

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-const app = require('./app');
+// database bn ulash uchun
 
-const port = 8003;
+// glabal ozgaruvchilarni tanish uchun
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
+// appni require qivoldik
+const app = require('./app.js');
+//bulani bilamiz
+const port = process.env.PORT;
 app.listen(port, '127.0.0.1');
+
+// console.log(app.get('env')); //-->qaysi environmentdagiligino korsatadi
+// console.log(process.env); //-->glabal ozgaruvchilar royhati
+
+//mongooseni ishlatish
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {}).then(() => {
+  console.log('DB connection succesful');
+});
+
+// sxema yaratib olish
+
+////////////////////////////////////////////
