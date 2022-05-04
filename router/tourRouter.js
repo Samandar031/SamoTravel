803a6d5b6216735fd452f5cd1748f541f777fad8
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-05-04 17:31:51 +0500

    a

--- router/tourRouter.js
+++ router/tourRouter.js
@@ -1,16 +1,37 @@
-const { json } = require('express/lib/response');
-const express = require('express');
+// const { json } = require('express/lib/response');
+// const express = require('express');
 
-const tourCountroller = require('../controller/tourCortroller.js');
+// const tourCountroller = require('../controller/tourCortroller.js');
+
+// const tourRouter = express.Router();
+
+// tourRouter
+//   .route('/')
+//   .get(tourCountroller.getTours)
+//   .post(tourCountroller.postTours);
+// tourRouter
+//   .route('/:id')
+//   .get(tourCountroller.getTour)
+//   .patch(tourCountroller.patchTours)
+//   .delete(tourCountroller.deleteTours);
+// //////////////////////////////////////////////
+
+const express = require('express');
+const tourController = require('../controller/tourController');
 
 const tourRouter = express.Router();
 
+// tourRouter.param('id', tourController.checkId);
+
 tourRouter
   .route('/')
-  .get(tourCountroller.getTours)
-  .post(tourCountroller.postTours);
+  .get(tourController.getToursAll)
+  .post(tourController.addTour);
+
 tourRouter
   .route('/:id')
-  .get(tourCountroller.getTour)
-  .patch(tourCountroller.patchTours)
-  .delete(tourCountroller.deleteTours);
+  .delete(tourController.deleteTour)
+  .get(tourController.getTourItem)
+  .patch(tourController.updateTour);
+
+module.exports = tourRouter;
